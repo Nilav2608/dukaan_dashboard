224ed70af790383264ded029ac36dd92c0653791
Nilavarasu2608 <knlsu444@gmail.com>
2024-05-08 01:26:10 +0530

    Side menu data

---
+++ lib/data/side_menu_data.dart
@@ -0,0 +1,37 @@
+import '../models/side_menu_model.dart';
+
+class SideMenuData {
+  final List<SideMenuModel> sideMenuData = [
+    SideMenuModel(
+        iconPath: 'assets/icons/home.svg', name: "Home", ),
+    SideMenuModel(
+        iconPath: 'assets/icons/orders.svg', name: "Orders",),
+    SideMenuModel(
+        iconPath: 'assets/icons/products.svg',
+        name: "Products",),
+    SideMenuModel(
+        iconPath: 'assets/icons/delivery.svg',
+        name: "Delivery",),
+    SideMenuModel(
+        iconPath: 'assets/icons/Marketing.svg',
+        name: "Marketing",),
+    SideMenuModel(
+        iconPath: 'assets/icons/analytics.svg',
+        name: "Analytics",),
+    SideMenuModel(
+        iconPath: 'assets/icons/Payouts.svg',
+        name: "Payouts",),
+    SideMenuModel(
+        iconPath: 'assets/icons/discounts.svg',
+        name: "Discounts",),
+    SideMenuModel(
+        iconPath: 'assets/icons/Audience.svg',
+        name: "Audience",),
+    SideMenuModel(
+        iconPath: 'assets/icons/Appearance.svg',
+        name: "Appearance",),
+    SideMenuModel(
+        iconPath: 'assets/icons/plugins.svg',
+        name: "Plugins",),
+  ];
+}
