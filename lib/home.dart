d17d0efd0ea7152b42cc7b46a3c24bc90b049917
Nilavarasu2608 <knlsu444@gmail.com>
2024-05-08 09:34:25 +0530

    Paouts widget

--- lib/home.dart
+++ lib/home.dart
@@ -1,22 +1,20 @@
+import 'package:dukaan_dashboard/pages/payouts/payouts.dart';
 import 'package:dukaan_dashboard/widgets/navigation_bar.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 
 class Home extends StatelessWidget {
   const Home({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return const Scaffold(
         body: Row(
       children: [
-        const Expanded(flex: 2, child: SideNavigationBar()),
+        Expanded(flex: 2, child: SideNavigationBar()),
         Expanded(
             flex: 9,
-            child: Container(
-              decoration: const BoxDecoration(color: Colors.white),
-              child: Column(),
-            ))
+            child: Payouts()
+        )
       ],
     ));
   }
