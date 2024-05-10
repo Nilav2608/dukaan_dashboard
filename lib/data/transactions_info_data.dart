c837a20d554c5cbea9ca77131bb185ed9d05d654
Nilavarasu2608 <knlsu444@gmail.com>
2024-05-11 00:30:40 +0530

    transactions data

--- lib/data/transactions_info_data.dart
+++ lib/data/transactions_info_data.dart
@@ -1,3 +1,4 @@
+import '../models/transaction_details_model.dart';
 import '../models/transactions_info_model.dart';
 
 class TransactionsInfoData {
@@ -7,5 +8,64 @@ class TransactionsInfoData {
     TransactionsInfoModel(transactionName: "Next Payout", counts: '2'),
     TransactionsInfoModel(transactionName: "Completed Payouts", counts: '14'),
     TransactionsInfoModel(transactionName: "Refunds", counts: '2'),
+  ];
+
+  final List<TransactionDetailsModel> transactions = [
+    TransactionDetailsModel(
+        date: 'Today, 09:00 PM',
+        status: "Processing",
+        transactionID: '131634495747',
+        orderAmount: '10,125.00',
+        transactionFees: '1,125.00',
+        total: '9,312'),
+    TransactionDetailsModel(
+        date: 'Today, 03:00 PM',
+        status: "Successful",
+        transactionID: '131634495747',
+        orderAmount: '10,125.00',
+        transactionFees: '1,125.00',
+        total: '9,312'),
+    TransactionDetailsModel(
+        date: 'Today, 09:00 AM',
+        status: "Successful",
+        transactionID: '131634495747',
+        orderAmount: '10,125.00',
+        transactionFees: '1,125.00',
+        total: '9,312'),
+    TransactionDetailsModel(
+        date: 'Yesterday, 3:00 PM',
+        status: "Successful",
+        transactionID: '131634495747',
+        orderAmount: '10,125.00',
+        transactionFees: '1,125.00',
+        total: '9,312'),
+    TransactionDetailsModel(
+        date: 'Yesterday, 09:00 AM',
+        status: "Successful",
+        transactionID: '131634495747',
+        orderAmount: '10,125.00',
+        transactionFees: '1,125.00',
+        total: '9,312'),
+    TransactionDetailsModel(
+        date: '12 Jul 2023, 03:00 PM',
+        status: "Successful",
+        transactionID: '131634495747',
+        orderAmount: '10,125.00',
+        transactionFees: '1,125.00',
+        total: '9,312'),
+    TransactionDetailsModel(
+        date: 'Today, 09:00 PM',
+        status: "Successful",
+        transactionID: '131634495747',
+        orderAmount: '10,125.00',
+        transactionFees: '1,125.00',
+        total: '9,312'),
+    TransactionDetailsModel(
+        date: '12 Jul 2023, 11:00 AM',
+        status: "Successful",
+        transactionID: '131634495747',
+        orderAmount: '10,125.00',
+        transactionFees: '1,125.00',
+        total: '9,312'),
   ];
 }
