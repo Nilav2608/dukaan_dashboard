2109403140bc1ac2125c81ebdf1915826aff400e
Nilavarasu2608 <knlsu444@gmail.com>
2024-05-11 14:06:22 +0530

    Credits widget card

--- lib/widgets/navigation_bar.dart
+++ lib/widgets/navigation_bar.dart
@@ -80,6 +80,66 @@ class _SideNavigationBarState extends State<SideNavigationBar> {
                     );
                   }),
             ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                  child: Container(
+                    width: 192,
+                    height: 54,
+                    decoration: BoxDecoration(
+                        color: const Color(0xff353C53),
+                        borderRadius: BorderRadius.circular(4)),
+                    child: Padding(
+                      padding:
+                          const EdgeInsets.only(left: 12, top: 6.0, bottom: 6),
+                      child: Row(
+                        children: [
+                          Container(
+                            width: 36,
+                            height: 36,
+                            decoration: BoxDecoration(
+                                color: const Color.fromARGB(37, 255, 255, 255),
+                                borderRadius: BorderRadius.circular(4)),
+                            child: Padding(
+                              padding: const EdgeInsets.symmetric(
+                                  horizontal: 6.0, vertical: 8),
+                              child:
+                                  SvgPicture.asset('assets/icons/wallet.svg'),
+                            ),
+                          ),
+                          const SizedBox(
+                            width: 12,
+                          ),
+                          const Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                "Available credits",
+                                style: TextStyle(
+                                    color: Color(0xffffffff),
+                                    fontSize: 13,
+                                    fontFamily: 'Inter',
+                                    fontWeight: FontWeight.w400),
+                              ),
+                              Text(
+                                "222.10",
+                                style: TextStyle(
+                                    color: Color(0xffffffff),
+                                    fontSize: 16,
+                                    fontFamily: 'Inter',
+                                    fontWeight: FontWeight.w500),
+                              ),
+                            ],
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                )
+              ],
+            )
           ],
         ),
       ),
